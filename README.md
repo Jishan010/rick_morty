40d8aab81a283752a1dfe7722a47589033a60c8c
jishan ansari <ansari_jishan@network.lilly.com>
2022-08-08 20:11:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Intro
+
+![Alt text](sample_rick.jpeg?raw=true "Title")
+
+
+This project uses Android studio 3.5 and targets api 28
+
+To run please sync with gradle to grab dependencies
+
+Rick Morty API was used in this implementation
+
+## Project Structure
+
+This follows a modular design, separating the presentation, domain, data layer
+it applies the use of interfaces as a binding contract to provide the minimum required
+feature target of a class.
+
+## Libraries
+
+- Ketro https://smilecs.github.io/ketro/ (Personal library available on my github and Jcenter) : For request wrapping and error handling
+- Coroutines : For background processing
+- Dagger : Dependency injection
+- Coil: For Image loading (Uses Coroutines internally)
+
+Please set your sdk location in local.properties file
+
+  
---
+++ presentation/src/main/java/com/smile/presentation/adapter/CharacterAdapter.kt
@@ -0,0 +1,30 @@
+package com.smile.presentation.adapter
+
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.smile.presentation.R
+import com.smile.presentation.uimodel.CharacterUI
+
+class CharacterAdapter(
+    private val items: List<CharacterUI>,
+    private val imageCache: (Int, Drawable) -> Unit,
+    private val clickAction: (CharacterUI) -> Unit
+) : RecyclerView.Adapter<CharacterAdapterViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterAdapterViewHolder {
+        Log.d("onCreateViewHolder", viewType.toString())
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return CharacterAdapterViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: CharacterAdapterViewHolder, position: Int) {
+        Log.d("onBindViewHolder", position.toString())
+        holder.bind(items[position], imageCache, clickAction)
+    }
+
+}
