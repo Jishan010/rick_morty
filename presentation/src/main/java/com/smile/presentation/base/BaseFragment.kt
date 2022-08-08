1eed035398da6684349e100aaf4fec42313f041b
jishan ansari <ansari_jishan@network.lilly.com>
2022-08-08 20:28:32 +0530

    removed extran blank space

--- presentation/src/main/java/com/smile/presentation/base/BaseFragment.kt
+++ presentation/src/main/java/com/smile/presentation/base/BaseFragment.kt
@@ -7,8 +7,6 @@ import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
 abstract class BaseFragment : DaggerFragment() {
-
     @Inject
     lateinit var viewModelFactory: Lazy<ViewModelProvider.Factory>
-
 }
